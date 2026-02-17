dcd241048c5a401924f9cad8b6bb919295456a7d
Thomas-9119 <dharrajparno@gmail.com>
2026-02-17 15:18:01 +0530

    com

---
+++ Telusko/06_set.py
@@ -0,0 +1,8 @@
+set1 = {23,56,19,23}
+print(set1)
+print(23 in set1)
+print(type(set1))
+set2 ={44,23,56,11,10}
+print(type(set2))
+print(set1 & set2)
+print(set1 | set2)
---
+++ Telusko/07_dictionary.py
@@ -0,0 +1,4 @@
+data = {0:34,1:35,'U':96,5:87}
+print(data['U'])
+print(data[0])
+print(data.get(0))
